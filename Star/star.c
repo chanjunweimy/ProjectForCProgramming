52915c240a89b62ca9f9558b870c8f80aaf633ce
Chan Jun Wei <chanjunweimy@gmail.com>
2015-09-29 16:23:52 +0800

    Done star
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

---
+++ Star/star.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int n;
+
+    printf("How many level of stars you want to see? Integer: \n");
+    scanf("%d", &n);
+
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
