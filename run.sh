e8ccffe25161c1a703b03ea329a7f38208ba6d58
quarbby <lynnette-ng@outlook.com>
2015-10-06 11:19:05 +0800

    Code Cov test

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 # Compile all the files
-make clean
+#make clean
 make
 
 cd 02_Seeing_Stars
 ./star_shell.sh
-
-# Run code coverage
-#for f in $(find . -name '*.c');
-#do
-#    echo $f;
-#    gcov $f;
-#done
+cd ..
