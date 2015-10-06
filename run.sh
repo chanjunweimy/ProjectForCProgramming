589fa179fcaebf7e5e5529b12b8712f955cc975b
quarbby <lynnette-ng@outlook.com>
2015-10-06 10:34:20 +0800

    Adding code coverage

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
+#!/bin/sh
+
+# Compile all the files
 make clean
 make
+
 cd 02_Seeing_Stars
 ./star_shell.sh
+
+# Run code coverage
+CFILES = find . -name '*.c'
+for f in $CFILES
+do
+    echo $f
+    gcov $f
+done
+
